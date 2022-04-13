6c969567cb4d714fb2b30bb1f177f3d8beb8187d
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2022-04-13 13:33:53 -0700

    Update file MEthod.rb

--- BASICS/file MEthod.rb
+++ BASICS/file MEthod.rb
@@ -4,7 +4,9 @@ File.open("employees.txt", "r") do |filer|
   puts filer.readline() # one line only
   puts filer.readlines() #store as array
 end
+File.close
 
 File.open("employees.txt", "r") do |filer|
     puts filer.readlines()[0,2]
 end
+File.close
