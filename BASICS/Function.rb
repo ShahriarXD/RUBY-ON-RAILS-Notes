9f0d871f7c27d8b8f40a722766e1f33fc609edd7
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2022-04-13 12:39:41 -0700

    Create Function.rb

---
+++ BASICS/Function.rb
@@ -0,0 +1,12 @@
+def function1
+    puts "Hello user"
+end
+
+function1()
+
+def function2(name,age)
+    puts ("Hello user"+name)
+    puts("And your age is "+age.to_s)
+end
+name = " Khanki Mgai"
+function2(name,73)
