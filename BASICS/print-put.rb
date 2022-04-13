2860ed3ee49bcf0e51b425c6717d37b459904b3b
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2022-04-13 11:10:20 -0700

    Update print-put.rb

--- BASICS/print-put.rb
+++ BASICS/print-put.rb
@@ -8,3 +8,24 @@ print "Khanki Magir Pola"
 khankimagir_pola = "Habib Shakil"
 age = "55"
 puts ("Hello " + khankimagir_pola + " your age is "+ age +".")
+
+
+
+
+
+
+
+
+
+#String MEthod
+
+name = "Mike will made it"
+occupation = "Sex Worker"
+name1 = '               Remove WhiteSpace              '
+age = 75
+cpa = 2.47
+
+puts name.upcase()
+puts name.length()
+puts name1.strip()
+puts name1.downcase()
