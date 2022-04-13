c2a5cd4c369b21132714a4ababa52baaf6a8bba7
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2022-04-13 13:15:33 -0700

    Create forLoop.rb

---
+++ BASICS/forLoop.rb
@@ -0,0 +1,11 @@
+friends = ["kevin", "Karebn" , "ADsad" , "Toyata"]
+for x in 0..2
+    puts("dasd")
+end
+
+
+
+friends = ["kevin", "Karebn" , "ADsad" , "Toyata"]
+for x in friends
+    puts x
+end
