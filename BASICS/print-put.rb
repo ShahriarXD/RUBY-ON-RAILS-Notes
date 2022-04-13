8383fb08726abba1e8312ebbb8e382b92105d709
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2022-04-13 10:53:01 -0700

    Update print-put.rb

--- BASICS/print-put.rb
+++ BASICS/print-put.rb
@@ -2,3 +2,9 @@ print "Khanki Magir Pola "
 puts "MIKE"  #print will be on new line and on put same line
 
 print "Khanki Magir Pola"
+
+
+
+khankimagir_pola = "Habib Shakil"
+age = "55"
+puts ("Hello " + khankimagir_pola + " your age is "+ age +".")
