900ce04319387f1f75fd1dd03fbf1f88a05ac076
Shahriar Hossain <73265380+ShahriarXD@users.noreply.github.com>
2022-04-13 12:25:26 -0700

    Create List.rb

---
+++ BASICS/List.rb
@@ -0,0 +1,9 @@
+list = Array[1,2,"Kevin",4,5,6]
+friends = Array.new
+friends[0] = "SAME"
+friends[1] = "SAM11E"
+puts (list)
+puts (list[2])
+puts(friends)
+puts(list.reverse())
+puts (list.sort())
